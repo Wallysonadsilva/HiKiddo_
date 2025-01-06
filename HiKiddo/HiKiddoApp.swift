3d39ef3d9c24a9cb9b83fdc1e4b145a9095cc177
Wally A Silva <wallyson-s@hotmail.com>
2025-01-06 22:58:14 +0000

    Initial Commit

---
+++ HiKiddo/HiKiddoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiKiddoApp.swift
+//  HiKiddo
+//
+//  Created by W on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HiKiddoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HiKiddoTests/HiKiddoTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiKiddoTests.swift
+//  HiKiddoTests
+//
+//  Created by W on 06/01/2025.
+//
+
+import Testing
+@testable import HiKiddo
+
+struct HiKiddoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
