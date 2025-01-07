0fbc93ca0150f2f49a5c3b6423a7017b7ac5afd6
Wally A Silva <wallyson-s@hotmail.com>
2025-01-07 00:03:43 +0000

    Created the Welcome View

---
+++ HiKiddo/Views/WelcomeView/WelcomeView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  HiKiddo
+//
+//  Created by W on 06/01/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @State private var navigateToAuth = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 30) {
+                Spacer()
+                
+                // Image
+                Image("Welcome_Image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250)
+                    .padding(.top, 50)
+                
+                // Welcome Message
+                Text("Welcome to HiKiddo!")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Your journey starts here")
+                    .font(.title3)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+                
+                // Start Button
+                NavigationLink(destination: AuthView()) {
+                    Text("Get Started")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.primaryPurple)
+                        .cornerRadius(12)
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 40)
+            }
+        }
+    }
+}
+
+// Preview
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
+
+// AuthView
+struct AuthView: View {
+    var body: some View {
+        VStack {
+            Text("Login/Register")
+                .font(.largeTitle)
+        }
+    }
+}
